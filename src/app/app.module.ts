2c5361c2e95acd4b07a390aa964a4f101c803e37
Nqobile <nqobilemoyo@capitecbank.co.za>
2022-05-04 13:02:36 +0200

    styling the look and feel of the website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
+import { AboutComponent } from './about/about.component';
 import { AppComponent } from './app.component';
+import { AuthGuard } from './auth.guard';
 import { BrowserModule } from '@angular/platform-browser';
+import { HomeComponent } from './home/home.component';
 import { NavComponent } from './nav/nav.component';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './routes';
 
 @NgModule({
-  declarations: [	
+  declarations: [			
     AppComponent,
-      NavComponent
+      NavComponent,
+      AboutComponent,
+      HomeComponent
    ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes),
   ],
-  providers: [],
+  providers: [
+    AuthGuard
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,9 +1,17 @@
+import { AboutComponent } from './about/about.component';
+import { AuthGuard } from './auth.guard';
+import { HomeComponent } from './home/home.component';
 import {Routes} from '@angular/router';
 
 export const appRoutes: Routes = [
+  { path: '', component: HomeComponent},
   {  
     path: '',
-    children: []
+    canActivate: [AuthGuard],
+    children: [
+      { path: 'about', component: AboutComponent, resolve: {}},
+    ]
   },
+  { path: '**', redirectTo: '', pathMatch: 'full'},
 ];
 
